3654e617fcc0c1e346046b6afb98f10e674c6367
Mark Fejes <fejes.mark92@gmail.com>
2018-01-14 17:24:49 +0100

    basic working

---
+++ dir-player.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+const { spawn } = require('child_process');
+const { readdirSync } = require('fs');
+
+const supportedFileExtensions = ['.mp4', '.mkv'];
+
+const filesToPlay = readdirSync(process.cwd())
+  .filter(file => supportedFileExtensions.some(ext => file.includes(ext)));
+
+if (filesToPlay.length === 0) {
+  process.exit();
+}
+
+let currentIndex;
+
+if (process.argv[2]) {
+  currentIndex = filesToPlay.indexOf(process.argv[2]);
+  if (currentIndex === -1) {
+    process.exit();
+  }
+} else {
+  currentIndex = 0;
+}
+
+
+function playFile(file) {
+  console.log(`Playing ${file}`);
+  const player = spawn('omxplayer', ['-b', file], {
+    stdio: 'inherit'
+  });
+  return player;
+}
+
+function onClose() {
+  if (currentIndex + 1 < filesToPlay.length) {
+    currentIndex++;
+    console.log(`Playing next file: ${filesToPlay[currentIndex]}`);
+    const player = playFile(filesToPlay[currentIndex]);
+    player.on('close', onClose);
+  } else {
+    console.log('Done');
+    process.exit();
+  }
+}
+
+const player = playFile(filesToPlay[currentIndex]);
+player.on('close', onClose);
